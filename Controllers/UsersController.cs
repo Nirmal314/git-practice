8c8f3f5fcc4010ae7c090ee4f3d97364228a8c6a
siddharthpd <sidpd510@gmail.com>
2024-09-25 12:41:45 +0530

    remove-changes

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -35,8 +35,6 @@ public class UsersController : ControllerBase
         {
             return NotFound();
         }
-        Console.WriteLine("hello world!");
-        Console.WriteLine("hello world!");
         return Ok(user);
     }
 
